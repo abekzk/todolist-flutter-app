c02ac58830f022edfa0fbf432d3917a765298b4c
kzuabe <ai3ekzk@gmail.com>
2023-01-15 01:04:00 +0900

    feat: 認証の管理をChangeNotifierProviderを利用した制御に変更

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+class AppUser {
+  String id;
+  String name;
+  String email;
+
+  AppUser({required this.id, required this.name, required this.email});
+}
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,8 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todolist_flutter_app/models/user.dart';
+import 'package:todolist_flutter_app/services/firebase/auth.dart';
 
-final firebaseAuthProvider =
-    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+class AuthState {
+  AppUser? user;
 
-final authStateChangesProvider = StreamProvider<User?>(
-    (ref) => ref.watch(firebaseAuthProvider).authStateChanges());
+  AuthState({required this.user});
+}
+
+class AuthNotifier extends ChangeNotifier {
+  final FirebaseAuth _auth;
+  final AuthState state = AuthState(user: null);
+
+  AuthNotifier(this._auth) {
+    _auth.authStateChanges().listen((user) {
+      if (user != null) {
+        state.user =
+            AppUser(id: user.uid, name: user.displayName!, email: user.email!);
+        notifyListeners();
+      } else {
+        state.user = null;
+        notifyListeners();
+      }
+    });
+  }
+
+  Future<void> signIn({required String email, required String password}) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signOut() {
+    return _auth.signOut();
+  }
+}
+
+final authProvider = ChangeNotifierProvider<AuthNotifier>(((ref) {
+  final auth = ref.watch(firebaseAuthProvider);
+  return AuthNotifier(auth);
+}));
